80441ae3a2540c1d01c77f30243276ee2e5a61c2
Elisa Covato <elisa.covato@gmail.com>
2018-06-22 13:32:38 +0100

    Added service worker

---
+++ sw.js
@@ -0,0 +1,61 @@
+// Adapted from https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+
+// Files that we want to catch
+var filesToCache = [
+  '.',
+  //cache css files
+  'css/media-queries.css',
+  'css/styles.css',
+  //cache images
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  //cache html
+  'index.html',
+  'restaurant.html',
+  //cache js files
+  'js/main.js',
+  'js/restaurant_info.js',
+  "js/dbhelper.js",
+  //cache data
+  'data/restaurants.json'
+];
+
+// Cache name. All the updatings happen here
+var staticCacheName = 'pages-cache-1';
+
+//We create the cache and we add the files to it.
+//We wrap the event in event.waitUntil so that the event terminates
+//when all the files are added to che cache succesfully
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .then(self.skipWaiting())
+  );
+});
+
+//We intercept requests for those files from the network
+// and respond with the files from the cache. 
+//Pages that have been visited got available offline
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request);
+    })
+  );
+});
